74351a332401dab0604b4f63358c347b8d1d37fa
Megan Gedde <hoglu070@d.umn.edu>
2022-04-03 10:17:21 -0500

    added hour blocks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,71 @@
 $("#currentDay").text(moment().format('dddd, MMMM Do'));
 
-var saveTasks = function() {
-    localStorage.setItem("tasks", JSON.stringify(tasks));
-  };
+//Change textarea background color based on time
+var checkTime = function () {
+    //Get Current time
+    var currentTime = moment().format('H');
+    //get all elements with class "textarea"
+    var timeBlockElements = $(".textarea");
 
-  var loadTasks = function() {
-    tasks = JSON.parse(localStorage.getItem("tasks"));
-  
-    // if nothing in localStorage, create a new object to track all task status arrays
-    if (!tasks) {
-      tasks = {
-        toDo: [],
-        inProgress: [],
-        inReview: [],
-        done: []
-      };
-    }
+    //loop through taskarea classes
+    for (var i = 0 ; i < timeBlockElements.length ; i++) {
+        var elementID = timeBlockElements[i].id;
+        var manipID = document.getElementById(timeBlockElements[i].id)
 
-var times = {};
+        //remove any old classes from element
+        $(timeBlockElements[i].id).removeClass(".present .past .future");
 
-var timeStart = 8am
-timeStart++
-
-for (var i = 8; i < 6; i++) {
-  
+        // apply new class if task is present/past/future
+        if (elementID < currentTime) {
+            $(manipID).addClass("past");
+        } else if (elementID > currentTime) {
+            $(manipID).addClass("future");
+        } else {
+            $(manipID).addClass("present");
+        }
     }
 }
+
+// checkTime every 15 minutes
+setInterval(checkTime(), (1000 * 60) * 15);
+
+checkTime()
+
+// $(".m-1").on("click", "p", function() {
+//     var text = $(this)
+//         .text()
+//         .trim();
+//     var textInput = $("<textarea>")
+//         .addClass("m-1")
+//         .val(text);
+//     $(this).replaceWith(textInput);
+//     textInput.trigger("focus")
+//     console.log(text);
+// })
+
+// var saveTasks = function() {
+//     localStorage.setItem("tasks", JSON.stringify(tasks));
+//   };
+
+//   var loadTasks = function() {
+//     tasks = JSON.parse(localStorage.getItem("tasks"));
+  
+//     // if nothing in localStorage, create a new object to track all task status arrays
+//     if (!tasks) {
+//       tasks = {
+//         toDo: [],
+//         inProgress: [],
+//         inReview: [],
+//         done: []
+//       };
+//     }
+
+// var times = {};
+
+// var timeStart = 8am
+// timeStart++
+
+// for (var i = 8; i < 6; i++) {
+  
+//     }
+// }
